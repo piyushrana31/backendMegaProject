9aac543ee60337fc6d79c8918849b8f2952254dd
piyushrana31 <prajput1236@gmail.com>
2025-12-01 22:59:42 +0530

    utils

--- src/app.js
+++ src/app.js
@@ -1,17 +1,21 @@
-import express, { urlencoded } from 'express';
+import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// express
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("Public"));
+
+//cors
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(express.static("Public"));
+// cookie-parser
 app.use(cookieParser());
 
 export { app };
